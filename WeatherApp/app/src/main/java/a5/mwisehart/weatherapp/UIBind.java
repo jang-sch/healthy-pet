af63ab4aaed054d728c2b85c5a72cd9e2dc70ef8
michaelhart2000 <michael.hart.booking@gmail.com>
2022-03-14 23:06:22 -0700

    a5 done

---
+++ WeatherApp/app/src/main/java/a5/mwisehart/weatherapp/UIBind.java
@@ -0,0 +1,5 @@
+package a5.mwisehart.weatherapp;
+
+public interface UIBind {
+    void mapUI(WeatherModel weatherModel);
+}
---
+++ WeatherApp2/app/src/main/java/a5/prodriguez20/weatherapp/UIBind.java
@@ -0,0 +1,5 @@
+package a5.prodriguez20.weatherapp;
+
+public interface UIBind {
+    void mapUI(WeatherModel weatherModel);
+}
