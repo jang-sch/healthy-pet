be0cccd9c3ec0638fbf470f80133225ad7d4980b
Jan <60149315+jang-sch@users.noreply.github.com>
2022-04-26 21:44:12 -0700

    fixed calling for pet header

--- api/mhpAPI.php
+++ api/mhpAPI.php
@@ -1,93 +1,61 @@
 <?php
-// 2022-04-08, 2022-04-13
-// main api file
+// this is the main api file
 
 require_once "userFuncs.php";
 
-// following line for debugging/building purposes
-require_once ".cnfg.php";
 
-
-
-/* for user to create account, requires React Native client to send data
-   note: in working order, now determine when sessionID will be generated
-*/
-if (isset($_POST['createAccount'])) {
-    header('Content-type: application/json');
+/* branch handles creation of new USER account. React Native Client sends 
+ * POST variables: "createAccount", username, email, password */
+if (isset($_POST["createAccount"])) {
+    header("Content-type: application/json");
     unset($_POST['createAccount']);
 
     // from React Native Client or curl request
-    $username = $_POST['username'];
-    $email = $_POST['email'];
-    $password = $_POST['password'];
+    $username = $_POST["username"];
+    $email = $_POST["email"];
+    $password = $_POST["password"];
 
     // verify required data prior to calling function/establishing connection
     if ($username == null || $password == null || $email == null) {
         //echo json_encode($userID);
-        echo json_encode(array("error" => "no username, email, or password sent."));
+        echo json_encode(array("error" => "no username/email/password sent."));
         return;
     } else {
-    // echo's back userID or error message as json encoded array
+
+    // echo's back key userID or key "error" message as json encoded array
     createUser();
     }
 }
 
-elseif (isset($_POST['createPet'])) {
+/* branch handles creation of new PET profile. React Native Client sends 
+ * POST variables: "createPet", userID, petName, speciesID, birthDate, sex
+ * michrochipNum, petPic */
+elseif (isset($_POST["createPet"])) {
     // in progress
 }
 
-// this branch is in progress
+/* branch handles obtaining pet header banner. React Native Client sends 
+ * POST variables: "petHeader", petID */
 elseif(isset($_POST['petHeader'])) {
-    //header('Content-type: application/json');
-    //unset($_POST['petHeader']);
-    //$petID = $_POST['petID'];
-    $conn = getConnection();
-
-    /* does not work when preparing a query to the pet_header *view* in the database
-       but does work when preparing queries that will happen on *tables* in the database. 
-       
-       using
-       curl -d "petHeader" -X POST https:...... on the target url
-
-       At this point, I was expecting the following results in array format:
-        +-------+-----------+----------+-------------+---------+
-        | petID | petName   | ageYears | speciesName | sex     |
-        +-------+-----------+----------+-------------+---------+
-        |  1024 | Banjo     | 6        | Cat         | Male    |
-        |  1025 | Daisy     | Unknown  | Dog         | Female  |
-        |  1026 | Buttercup | 1        | Dog         | Female  |
-        |  1027 | Rocco     | 1        | Cat         | Male    |
-        |  1028 | Scarlete  | 1        | Cat         | Female  |
-        |  1029 | Isaiah    | Unknown  | Cat         | Male    |
-        |  1036 | Champ     | Unknown  | Dog         | Unknown |
-        |  1037 | Champ Jr  | Unknown  | Dog         | Unknown |
-        +-------+-----------+----------+-------------+---------+
-
-    */
-    $result = $conn->prepare("SELECT * FROM pet_header");
-    $result->execute();
-
-    $petHeaders = $result->get_result();
-
-    $rows = [];
-    while($row = $petHeaders->fetch_assoc()) {
-        $rows [] = $row;
-    }
-
-    echo json_encode($rows);
-
+    header('Content-type: application/json');
+    unset($_POST['petHeader']);
+    // echos back keys petName, ageYears, speciesName, sex, as json encoded array
+    petHeader();
 }
 
-elseif(isset($_POST['tostada'])) {
+/* for React Native to test connection to API. React Native Client sends POST
+ * variable "tostada" and gets back keys "id" and "response" as json arrays */
+elseif(isset($_POST["tostada"])) {
     $result = [];
-    $result []= array("id" => 1, "response" => "tostada \u{1F920}");
+    $result []= array("id" => 1, "response" => "you requested a tostada  \u{1F32E}");
     echo json_encode($result);
 }
 
+/* no POST var sent */
 else {
     $result = [];
-    // array as array = [[1, "text"], [2, "more text"], [3, "more text"]]
-    $result []= array("id" => 1, "myResponse" => "no special tostada request, this will break my heart  \u{1F920}");
+    // [key, "values blah"] ie as array = [[1, "text"], [2, "more text"], [3, "more more text"]]
+    $result []= array("id" => 1, "myResponse" => "no special tostada request, this will break my heart  \u{1F624}");
     echo json_encode($result);
 }
 
