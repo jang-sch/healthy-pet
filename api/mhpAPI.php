2103ead01dd07a54658b1b4c37b04e8404e340e6
Jan <60149315+jang-sch@users.noreply.github.com>
2022-05-06 21:39:50 -0700

    refactored createPet from file to userFuncs.php

--- api/mhpAPI.php
+++ api/mhpAPI.php
@@ -31,75 +31,45 @@ if (isset($_POST["createAccount"])) {
     }
 }
 
+/* branch handles LOGGING IN. React Native Client sends POST variables: 
+ * "login", username, email, password */
+elseif (isset($_POST["login"])) {
+    header("Content-type: application/json");
+    unset($_POST["login"]);
+
+    // from React Native Client or curl request
+    $em = $_POST["email"];
+    $password = $_POST["password"];
+
+    // verify required data prior to calling function/establishing connection
+    if ($password == null || $em == null) {
+        //echo json_encode($userID);
+        echo json_encode(array("error" => "CANNOT LOGIN, no email/password sent"));
+        return;
+    } else {
+        // returns json encoded array for now
+        loginUser();
+    }
+
+    //echo json_encode("at the login end"); //debug
+}
+
 /* branch handles creation of new PET profile. React Native Client sends 
  * POST variables: "createPet", userID, petName, speciesID, birthDate, sex
  * microchipNum, petPic */
 elseif (isset($_POST["createPet"])) {
-    // in progress
     header("Content-type: application/json");
     unset($_POST["createPet"]);
 
-    // debugging:
-    $conn = getConnection();
-    // from React Native Client or curl request
-    /*
     $userID = $_POST["userID"];
-    $petName = $_POST["petName"];
-    $speciesID = $_POST["speciesID"];
-    $birthDate = $_POST["birthDate"];
-    $sex = $_POST["sex"];
-    $microchipNum = $_POST["microchipNum"];
-    */
-    
-    //$petPic = $_POST["petPic"];
 
-
-    // check if any are null:
-    $pet_data["userID"] = $_POST["userID"];
-    $pet_data["petName"] = $_POST["petName"];
-    $pet_data["speciesID"] = $_POST["speciesID"];
-    $pet_data["birthDate"] = $_POST["birthDate"];
-    $pet_data["sex"] = $_POST["sex"];
-    $pet_data["microchipNum"] = $_POST["microchipNum"];
-    // ***incorporate the  PIC ** but functional for now
-    $pet_data["petPic"] = NULL;
-
-    foreach($pet_data as $key => $val) {
-        //echo $key." is ".$val."\n";
-        if ($val == "") {
-            $pet_data[$key] = NULL;
-        }
-        //echo $key." issss ".$val."\n";
-    }
-
-    $createPet = $conn->prepare("CALL createPetProfile(?, ?, ?, ?, ?, ?, ?)");
-
-    // bind parameters, in variatic function, 's' for strings, 'i' for integers
-    $createPet->bind_param("isisisb", $pet_data["userID"], $pet_data["petName"], $pet_data["speciesID"],$pet_data["birthDate"],$pet_data["sex"],$pet_data["microchipNum"],$pet_data["petPic"]);
-
-    // now execute prepared stament
-    if($createPet->execute()){
-        //echo json_encode(array("testmssg" => "testing123")); // for debugging
-        // binding results to local variables so can fetch them and see if null
-        mysqli_stmt_bind_result($createPet, $res_id, $res_error);
-        $createPet->fetch();
-
-        // do we have successful registratation--> check for duplicate email
-        if (is_null($res_id)) {
-            // error code/error message from the database
-            echo json_encode(array("error" => $res_error));
-            return;
-        }
-        // all is well, registration worked and you are given a user ID return
-        else {
-            echo json_encode(array("petID" => $res_id));
-            //echo json_encode(array("username" => $res_id));
-            return;
-        }
+    // determine if a userID was sent prior to establishing connection
+    if($userID == NULL || $userID == "") {
+        echo json_encode("ERROR! Cannot create pet, no user ID sent!!");
     } else {
-        echo json_encode(array("error" => "an error has occurred!"));
+        // ready to call create pet
+        createPet();
     }
-
 }
 
 /* -------------------------------------GET VIEWS--------------------------- */
@@ -108,9 +78,9 @@ elseif (isset($_POST["createPet"])) {
  * POST variables: "petHeader", petID */
 elseif(isset($_POST["userProfile"])) {
     header("Content-type: application/json");
-    unset($_POST["petHeader"]);
+    unset($_POST["userProfile"]);
     // echos back keys petName, ageYears, speciesName, sex, as json encoded array
-    petHeader();
+    //petHeader();
 }
 
 /* branch handles obtaining pet header banner. React Native Client sends 
@@ -122,23 +92,25 @@ elseif(isset($_POST["petHeader"])) {
     petHeader();
 }
 
+/* -------------------------------------- OTHER ---------------------------- */
+
 /* for React Native to test connection to API. React Native Client sends POST
  * variable "tostada" and gets back keys "id" and "response" as json arrays */
-elseif(isset($_POST["tostada"])) {
+//elseif(isset($_POST["tostada"]==true)) {
+ elseif(isset($_POST["tostada"])) {
+    header("Content-type: application/json");
+    unset($_POST["tostada"]);
     $result = [];
-    $result []= array("id" => 1, "response" => "you requested a tostada  \u{1F32E}");
+    $result []= array("id" => 1, "response" => "SUCCESS! You requested a tostada  \u{1F32E}");
     echo json_encode($result);
 }
 
-/* -------------------------------------- OTHER ---------------------------- */
-
-/* no POST var sent */
+/* no POST keyword sent, so fall into this branch */
 else {
     $result = [];
     // [key, "values blah"] ie as array = [[1, "text"], [2, "more text"], [3, "more more text"]]
     $result []= array("id" => 1, "myResponse" => "no special tostada request, this will break my heart  \u{1F624}");
     echo json_encode($result);
 }
-
 
 ?>
